c5bbec2c7dda1a162ed0707bee5c06e03c1cd433
jhonatan vieira <jhonatanvieira@live.com.ar>
2020-08-13 13:32:55 -0300

    Instalación de react-native-video para visualizar video capturado. -Prueba de filmación con react-native-image-picker

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,20 +3,23 @@ import {
   View,
   Text,
   StyleSheet,
-  TouchableOpacity,
-  Image,
-  alert,
+  //TouchableOpacity,
+  //Image,
+  //alert,
+  Button,
 } from 'react-native';
 import FormButton from '../components/FormButton';
 import {AuthContext} from '../navigation/AuthProvider';
 import ImagePicker from 'react-native-image-picker';
+import Video from 'react-native-video';
 
 //Una vez en el home, puedo acceder a los datos del usuario por medio del state user
 export default function HomeScreen() {
   const {user, logout} = useContext(AuthContext);
-  const [resourcePath, setResourcePath] = useState({});
+  //const [resourcePath, setResourcePath] = useState({});
+  const [videoSource, setVideoSource] = useState('');
 
-  const selectFile = () => {
+  /*const selectFile = () => {
     var options = {
       title: 'Seleccione una imagen',
       //mediaType: 'video',
@@ -48,6 +51,32 @@ export default function HomeScreen() {
         setResourcePath(source);
       }
     });
+  };*/
+
+  const selectVideo = () => {
+    const options2 = {
+      title: 'Select video',
+      mediaType: 'video',
+      //durationLimit: 15,
+      path: 'video',
+      quality: 1,
+    };
+
+    ImagePicker.showImagePicker(options2, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = {uri: response.uri};
+
+        setVideoSource(source);
+      }
+    });
   };
 
   return (
@@ -55,7 +84,7 @@ export default function HomeScreen() {
       <Text style={styles.text}>Welcome user {user.uid}</Text>
       <FormButton buttonTitle="Logout" onPress={() => logout()} />
 
-      <Image
+      {/*<Image
         source={{
           uri: 'data:image/jpeg;base64,' + resourcePath.data,
         }}
@@ -69,7 +98,22 @@ export default function HomeScreen() {
 
       <TouchableOpacity onPress={selectFile} style={styles.button}>
         <Text>Select File</Text>
-      </TouchableOpacity>
+      </TouchableOpacity>*/}
+
+      <Video
+        source={videoSource} // Can be a URL or a local file.
+        //ref={(ref) => {
+        //  this.player = ref;
+        //}} // Store reference
+        //onBuffer={this.onBuffer} // Callback when remote video is buffering
+        //onError={this.videoError} // Callback when video cannot be loaded
+        style={styles.backgroundVideo}
+        controls={true}
+        fullscreen={true}
+        //style={styles.uploadImage}
+      />
+
+      <Button title="Select video" onPress={selectVideo} />
     </View>
   );
 }
@@ -84,5 +128,12 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     color: '#333333',
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
