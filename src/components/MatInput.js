19403814bbbed915eab2608a6e2e8beaf0b6c646
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-08-17 17:01:23 -0300

    Agregado componente MatInput, implementacion de MatUI-TextField para FC

---
+++ src/components/MatInput.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
+import {TextField} from 'react-native-material-textfield';
+import StylesConfiguration from '../utils/StylesConfiguration';
+
+/* 
+  Por algun motivo que no logro descifrar, TextField solo funciona con componentes declarados como clases
+*/
+
+class MatInput extends Component {
+  constructor(props) {
+    super(props);
+    console.log("created", props);
+  }
+
+  render() {
+    return (
+      <TextField
+        {...this.props}
+        textColor={'rgb(255,255,255)'}
+        tintColor={StylesConfiguration.color}
+        baseColor={StylesConfiguration.color}
+        labelTextStyle={styles.title}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'rgb(255,255,255)',
+  },
+  input: {
+    width: 50,
+    height: 20,
+  },
+});
+
+export default MatInput;
