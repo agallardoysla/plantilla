7aa8d425faa999b3865921fd0f30db68be6a206c
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-08-17 18:12:28 -0300

    Agregado boton de 'visibilidad' de contraseña

--- src/components/MatInput.js
+++ src/components/MatInput.js
@@ -1,27 +1,75 @@
 import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {TextField} from 'react-native-material-textfield';
 import StylesConfiguration from '../utils/StylesConfiguration';
+import { Icon } from 'react-native-elements';
+
 
 /* 
   Por algun motivo que no logro descifrar, TextField solo funciona con componentes declarados como clases
 */
 
 class MatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showIcon: this.props.secureTextEntry,
+      showPassword: false,
+    };
+  }
+
+  handleClickShowPassword = () => {
+    console.log(this.state.showPassword);
+    this.setState({showPassword: !this.state.showPassword});
+  }
+
   render() {
-    return (
-      <TextField
-        {...this.props}
-        textColor={'rgb(255,255,255)'}
-        tintColor={StylesConfiguration.color}
-        baseColor={StylesConfiguration.color}
-        labelTextStyle={styles.title}
-      />
-    );
+    if (this.state.showIcon) {
+      return (
+        <View style={styles.container}>
+          <TextField
+            {...this.props}
+            textColor={'rgb(255,255,255)'}
+            tintColor={StylesConfiguration.color}
+            baseColor={StylesConfiguration.color}
+            labelTextStyle={styles.title}
+            secureTextEntry={this.state.showIcon && !this.state.showPassword}
+          />
+          {this.state.showPassword ? (
+            <Icon
+              onPress={this.handleClickShowPassword}
+              name="visibility-off"
+              color="#FFFFFF"
+            />
+          ) : (
+            <Icon
+              onPress={this.handleClickShowPassword}
+              name="visibility"
+              color="#FFFFFF"
+            />
+          )}
+        </View>
+      );
+    } else {
+      return (
+        <TextField
+          {...this.props}
+          textColor={'rgb(255,255,255)'}
+          tintColor={StylesConfiguration.color}
+          baseColor={StylesConfiguration.color}
+          labelTextStyle={styles.title}
+        />
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   title: {
     color: 'rgb(255,255,255)',
   },
