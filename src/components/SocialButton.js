653b38e6baf143fa68c919c8736df9d7629c15b6
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-08-17 17:06:21 -0300

    Agregados los botones Facebook y Google

--- src/components/SocialButton.js
+++ src/components/SocialButton.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, Text} from 'react-native';
+import {StyleSheet, TouchableOpacity, Text, Image} from 'react-native';
 import StylesConfiguration from '../utils/StylesConfiguration';
 
-export default function SocialButton({buttonTitle, ...props}) {
+
+export function FacebookButton({buttonTitle, ...props}) {
   return (
     <TouchableOpacity
       style={styles.buttonContainer}
       activeOpacity={0.5}
       {...props}>
-      <Text style={styles.buttonText}>{buttonTitle}</Text>
+      <Image
+        style={styles.icon}
+        source={require('../assets/facebook-icon.png')}
+      />
+    </TouchableOpacity>
+  );
+}
+
+export function GoogleButton({buttonTitle, ...props}) {
+  return (
+    <TouchableOpacity
+      style={styles.buttonContainer}
+      activeOpacity={0.5}
+      {...props}>
+      <Image
+        style={styles.icon}
+        source={require('../assets/google-icon.png')}
+      />
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    marginTop: 22,
-    width: 200,
-    height: 43,
+    width: 50,
+    height: 50,
     backgroundColor: 'black',
-    padding: 10,
+    marginLeft: 15,
+    marginRight: 15,
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: '#E9FC64',
-    borderStyle: 'solid',
   },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: StylesConfiguration.fontWeight,
-    fontFamily: StylesConfiguration.fontFamily,
-    color: 'white',
+  icon: {
+    width: 50,
+    height: 50,
   },
 });
