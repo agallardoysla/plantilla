9fb861b9013481ab70e7835260711c40c5d838d9
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-09-01 12:06:34 -0300

    Agregados servicios de posts

---
+++ src/services/posts_services.js
@@ -0,0 +1,13 @@
+import generic_service from './generic_service';
+
+const url = 'posts/';
+
+export default {
+  list: () => generic_service.doGet(url),
+  get: (id) => generic_service.doGet(`${url}${id}/`),
+  edit: (id, newProfile) => generic_service.doPut(`${url}${id}/`, newProfile),
+  create: (newPost) => generic_service.doPost(url, newPost),
+  delete: (id) => generic_service.doDelete(`${url}${id}/`),
+  getComments: (id) => generic_service.doGet(`${url}${id}/comments/`),
+  getReactions: (id) => generic_service.doGet(`${url}${id}/reactions/`),
+};
