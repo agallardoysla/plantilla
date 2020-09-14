e8e715b57c7a18ba0ef3bd01524def25896b9971
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-09-14 10:35:52 -0300

    Al pedir los posts, se anidan las repsuestas de los comentarios

--- src/services/posts_services.js
+++ src/services/posts_services.js
@@ -4,7 +4,18 @@ const url = 'posts/';
 const pageSize = 20;
 
 export default {
-  list: (page) => generic_service.doGet(`${url}?limit=${pageSize}&offset=${page*pageSize}`),
+  list: async (page) => {
+    const res = await generic_service.doGet(`${url}?limit=${pageSize}&offset=${page*pageSize}`);
+    res.data = res.data.map(post => {
+      post.comments.map(c => {
+        c.comments = post.comments.filter(_c => _c.original_comment === c.id);
+        return c;
+      });
+      return post;
+    });
+    console.log(res.data[0]);
+    return res;
+  },
   get: (id) => generic_service.doGet(`${url}${id}/`),
   edit: (id, newPost) => generic_service.doPut(`${url}${id}/`, newPost),
   create: (newPost) => generic_service.doPost(url, newPost),
