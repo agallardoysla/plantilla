98a1495086d9c1db1ac59bc454749a7dac37dc5f
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-09-16 15:26:14 -0300

    Adecuacion de servicios de post a nueva API

--- src/services/posts_services.js
+++ src/services/posts_services.js
@@ -4,18 +4,7 @@ const url = 'posts/';
 const pageSize = 20;
 
 export default {
-  list: async (page) => {
-    const res = await generic_service.doGet(`${url}?limit=${pageSize}&offset=${page*pageSize}`);
-    res.data = res.data.map(post => {
-      post.comments.map(c => {
-        c.comments = post.comments.filter(_c => _c.original_comment === c.id);
-        return c;
-      });
-      return post;
-    });
-    console.log(res.data[0]);
-    return res;
-  },
+  list: (page) => generic_service.doGet(`${url}?limit=${pageSize}&offset=${page*pageSize}`),
   get: (id) => generic_service.doGet(`${url}${id}/`),
   edit: (id, newPost) => generic_service.doPut(`${url}${id}/`, newPost),
   create: (newPost) => generic_service.doPost(url, newPost),
