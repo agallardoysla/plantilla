a9aa1ed546173d6aee9971949ebf9357cd5c8aeb
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-09-29 23:52:06 -0300

    Agregadas acciones de mostrar alert en la pantalla de preferencias

--- src/screens/profile/Preferences.js
+++ src/screens/profile/Preferences.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, Alert} from 'react-native';
 import StylesConfiguration from '../../utils/StylesConfiguration';
 import FormButton from '../../components/FormButton';
 import { AuthContext } from '../../navigation/AuthProvider';
@@ -14,13 +14,13 @@ export default function Preferences({navigation}) {
         source={require('../../assets/tuerca_blanca_grande.png')}
       />
       <Text style={styles.text}>PREFERENCIAS</Text>
-      <FormButton buttonTitle="Editar perfil" style={styles.action} />
-      <FormButton buttonTitle="Mi cuenta" style={styles.action} />
-      <FormButton buttonTitle="Privacidad" style={styles.action} />
-      <FormButton buttonTitle="Seguridad" style={styles.action} />
-      <FormButton buttonTitle="Condiciones y política de uso" style={styles.action} />
+      <FormButton buttonTitle="Editar perfil" style={styles.action} onPress={() => Alert.alert("editar perfil")} />
+      <FormButton buttonTitle="Mi cuenta" style={styles.action} onPress={() => Alert.alert("mi cuenta")} />
+      <FormButton buttonTitle="Privacidad" style={styles.action} onPress={() => Alert.alert("privacidad")} />
+      <FormButton buttonTitle="Seguridad" style={styles.action} onPress={() => Alert.alert("seguridad")} />
+      <FormButton buttonTitle="Condiciones y política de uso" style={styles.action} onPress={() => Alert.alert("condiciones")} />
       <FormButton buttonTitle="Logout" style={styles.action} onPress={logout} />
-      <FormButton buttonTitle="Eliminar cuenta" style={styles.action} />
+      <FormButton buttonTitle="Eliminar cuenta" style={styles.action} onPress={() => Alert.alert("eliminar cuenta")} />
     </View>
   );
 }
