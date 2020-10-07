41f70a7ee11a13fa9b74cfa5f32aee53903e400c
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-10-06 22:30:31 -0300

    Configuracion basica de websockets (validado con BE)

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,10 +3,8 @@ import {AuthProvider} from './AuthProvider';
 import Routes from './Routes';
 import { FeedProvider } from './FeedContext';
 import { MenuProvider } from 'react-native-popup-menu';
-import websocket_client from '../services/websocket_client';
 
 export default function Providers() {
-  websocket_client.init();
 
   return (
     <MenuProvider>
--- src/services/websocket_client.js
+++ src/services/websocket_client.js
@@ -1,31 +1,31 @@
-var WebSocket = require('ws');
-import api_config from './api_config';
-import auth from '@react-native-firebase/auth';
+// import api_config from './api_config';
+// import auth from '@react-native-firebase/auth';
 
-const url = api_config.webSocketUrl;
+// const url = api_config.webSocketUrl;
 
-const init = async () => {
-  const token = await auth().currentUser.getIdToken(true);
+// const init = async () => {
+//   console.log('Init WS');
+//   const token = await auth().currentUser.getIdToken(true);
 
-  const headers = {
-    Authorization: `JWT ${token}`,
-  };
+//   const headers = {
+//     Authorization: `JWT ${token}`,
+//   };
+  
+//   const ws = new WebSocket(url, '', {origin: url, headers: headers});
 
-  const ws = new WebSocket(url, {origin: url, headers: headers});
+//   ws.onopen = () => {
+//     console.log('connected');
+//   };
 
-  ws.on('open', function open() {
-    console.log('connected');
-  });
+//   ws.on('close', function close() {
+//     console.log('disconnected');
+//   });
 
-  ws.on('close', function close() {
-    console.log('disconnected');
-  });
+//   ws.onmessage = function incoming(data) {
+//     console.log("message", data);
+//   };
+// };
 
-  ws.on('message', function incoming(data) {
-    console.log("message", data);
-  });
-};
-
-export default {
-  init,
-};
+// export default {
+//   init,
+// };
