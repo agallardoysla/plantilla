d096c8f582279975f4143223ffa3f90af6041e45
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-10-14 10:01:49 -0300

    Creado componente boton de ir atras

---
+++ src/components/GoBackButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Image, StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+export default function GoBackButton({navigation, _styles}) {
+  return (
+    <TouchableOpacity
+      onPress={navigation.goBack}
+      style={[styles.container, _styles]}>
+      <Image
+        style={styles.boton_back}
+        source={require('../assets/boton_volver_atras.png')}
+      />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 30,
+    width: 30,
+  },
+  boton_back: {
+    height: 30,
+    width: 30,
+  },
+});
