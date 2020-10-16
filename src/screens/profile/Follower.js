d4892801739d6f9f5f52465800e7ab6843bc41cd
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-10-16 10:23:11 -0300

    Dejado la pantalla de Seguidores similar a la maqueta

--- src/screens/profile/Follower.js
+++ src/screens/profile/Follower.js
@@ -8,86 +8,99 @@ import StylesConfiguration from '../../utils/StylesConfiguration';
 const Follower = ({follower, navigation}) => {
   const [userFollowProfile, setUserFollowProfile] = useState(false);
 
-  const goToProfile = (profile) => {
+  const goToProfile = () => {
     navigation.navigate('HomeGroup', {
       screen: 'OtherProfile',
       params: {
-        user_id: profile.id,
+        user_id: follower.id,
         navigation,
       },
     });
   };
 
-  const goToMyChat = (profile) => {
+  const goToMyChat = () => {
     navigation.navigate('MyChat', {
       receiver: {
-        user_id: profile.id,
-        display_name: profile.display_name,
+        user_id: follower.id,
+        display_name: follower.display_name,
       },
     });
   };
 
   return (
-    <View style={styles.row_content}>
-      <Image
-        source={require('../../assets/pride-dog_1.png')}
-        resizeMode="contain"
-        style={styles.image}
-      />
-      <Text style={styles.text}>@{follower.display_name}</Text>
-      <TouchableOpacity
-        style={styles.sobre_amarillo}
-        onPress={() => goToMyChat(follower)}>
+    <View style={styles.row}>
+      <TouchableOpacity onPress={goToProfile} style={styles.user}>
         <Image
-          source={require('../../assets/sobre_amarillo.png')}
-          style={styles.sobre_amarillo}
+          source={require('../../assets/pride-dog_1.png')}
+          resizeMode="contain"
+          style={styles.image}
         />
+        <Text style={styles.profileName} numberOfLines={2}>
+          @{follower.display_name}
+        </Text>
       </TouchableOpacity>
-      <FormButton
-        buttonTitle={userFollowProfile ? 'Seguido' : 'Seguir'}
-        style={styles.followButton}
-        textStyle={styles.followButtonText}
-      />
-      <FormButton
-        buttonTitle={'...'}
-        style={styles.followButton}
-        textStyle={styles.followButtonText}
-      />
+      <View style={styles.actions}>
+        <TouchableOpacity
+          style={styles.sobre_amarillo}
+          onPress={goToMyChat}>
+          <Image
+            source={require('../../assets/sobre_amarillo.png')}
+            style={styles.sobre_amarillo}
+          />
+        </TouchableOpacity>
+        <FormButton
+          buttonTitle={userFollowProfile ? 'Seguido' : 'Seguir'}
+          style={styles.followButton}
+          textStyle={styles.followButtonText}
+        />
+        <FormButton
+          buttonTitle={'...'}
+          style={styles.followButton}
+          textStyle={styles.followButtonText}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  column: {
-    flex: 1,
-    flexDirection: 'column',
-    alignContent: 'center',
-    alignItems: 'flex-start',
-    backgroundColor: 'black',
-  },
-  row_content: {
+  row: {
     flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'space-between',
-
-    width: window.width,
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  user: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
   },
   image: {
     width: 60,
     height: 60,
-    marginBottom: 10,
-    borderRadius: 400 / 2,
   },
-  text: {
-    fontFamily: StylesConfiguration.fontFamily,
+  profileName: {
     color: 'white',
+    fontFamily: StylesConfiguration.fontFamily,
+    fontSize: 15,
+    width: 120,
+    marginLeft: 5,
+    overflow: 'hidden',
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   followButton: {
     width: 50,
     marginTop: 0,
     padding: 5,
     marginHorizontal: 5,
-    height: 35,
+    height: 30,
+    borderRadius: 5,
   },
   followButtonText: {
     fontSize: 10,
