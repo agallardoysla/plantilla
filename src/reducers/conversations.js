66960283f34ecb463d8d11a08e6352fd52b959d4
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-10-30 11:42:21 -0300

    Incorporadas acciones para agregar mensajes nuevos a conversaciones y para agregar conversaciones

--- src/reducers/conversations.js
+++ src/reducers/conversations.js
@@ -8,23 +8,51 @@ export const conversationsSlice = createSlice({
       conversations = action.payload;
       return conversations;
     },
-    addNotification: (conversations, action) => {
-      conversations = [...conversations, action.payload];
-      return conversations;
-    },
     resetConversations: (conversations) => {
       conversations = [];
       return conversations;
+    },
+    addConversation: (conversations, action) => {
+      conversations = [...conversations, action.payload];
+    },
+    pushMessage: (conversations, action) => {
+      conversations = conversations.map((conversation) => {
+        if (
+          conversation.active_users[0] === action.payload.from.user_id ||
+          conversation.active_users[1] === action.payload.from.user_id
+        ) {
+          const newMessage = {
+            ...action.payload,
+            from: action.payload.from.user_id,
+          };
+          conversation.messages.unshift(newMessage);
+        }
+        return conversation;
+      });
+      // return conversations;
     },
   },
 });
 
 export const {
   setConversations,
-  addNotification,
+  addConversation,
+  pushMessage,
   resetConversations,
 } = conversationsSlice.actions;
 
 export const getConversations = (state) => state.conversations;
+
+export const getConversationByParams = (conversationId, userId) => {
+  if (conversationId) {
+    return (state) => state.conversations.filter((c) => c.id === conversationId)[0];
+  }
+  if (userId) {
+    return (state) =>
+      state.conversations.filter((c) =>
+        c.active_users.reduce((r, au) => r || au === userId, false)[0],
+      );
+  }
+};
 
 export default conversationsSlice.reducer;
