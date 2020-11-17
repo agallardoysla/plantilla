8e3da46bc377fccfecfdd407aec9a08bc6f7ad87
juan.93.ctm@gmail.com <juan.93.ctm@gmail.com>
2020-11-17 09:25:08 -0300

    Merge

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,32 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { adapt } from '../utils/normalizedDataAdaptator';
 
-<<<<<<< HEAD
-export const usersSlice = createSlice({
-  name: 'users',
-  initialState: [],
-  reducers: {
-    setUsers: (users, action) => {
-      users = action.payload;
-      return users;
-    },
-    addUsers: (users, action) => {
-      users = [...users, ...action.payload];
-      return users;
-    },
-    resetUsers: (users) => {
-      users = [];
-      return users;
-    },
-  },
-});
-
-export const {setUsers, addUsers, resetUsers} = usersSlice.actions;
-
-export const getUsers = (state) => state.users.allIds;
-export const getUser = (id) => (state) => state.users.byId[id.toString()];
-
-=======
 const initialState = {
   byId: {},
   allIds: [],
@@ -59,5 +33,4 @@ export const getUsers = (state) => state.users.allIds;
 export const getFullUsers = (state) => state.users.byId;
 export const getUser = (id) => (state) => state.users.byId[id.toString()];
 
->>>>>>> integracion
 export default usersSlice.reducer;
