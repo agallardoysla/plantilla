ed2a1643b654bf764c7dab120e9d5c818d522944
rubench0 <rubenchoo.garcia@gmail.com>
2020-11-29 16:32:18 -0400

    hot-fix/resuelto conflicto con profile

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,14 +3,7 @@ import session from './session';
 import feed from './feed';
 import postDetails from './postDetails';
 import otherProfile from './otherProfile';
-
-export default combineReducers({session, feed, postDetails, otherProfile});
-
 import conversations from './conversations';
 
-export default combineReducers({
-  session, 
-  feed, 
-  postDetails,
-  conversations,
-});
+export default combineReducers({session, feed, postDetails, otherProfile, conversations});
+
